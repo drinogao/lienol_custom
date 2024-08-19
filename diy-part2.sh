b28d749d9abd3ca55cd9eed83bb4c8d6767ed57e
Drino <qq418484008@outlook.com>
2024-08-19 16:38:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,8 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,sm
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+sed -i "25i sed -i '/small/d' /etc/opkg/distfeeds.conf" package/default-settings/files/zzz-default-settings
+sed -i "26i sed -i '/kenzo/d' /etc/opkg/distfeeds.conf" package/default-settings/files/zzz-default-settings
+sed -i '27i echo "src/gz openwrt_smpackage https://op.dllkids.xyz/packages/x86_64/" >> /etc/opkg/customfeeds.conf' package/default-settings/files/zzz-default-settings
+sed -i "28i sed -i '/signature/d' opkg.conf"  package/default-settings/files/zzz-default-settings
+sed -i '29i echo "#option check_signature" >> /etc/opkg.conf' package/default-settings/files/zzz-default-settings
